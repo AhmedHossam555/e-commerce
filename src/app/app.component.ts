095ae29ee789fc1466a6f4db58e06511a1111053
ahmed hossam <ahmed.hossam.elgamal77@gmail.com>
2024-08-28 15:32:40 +0300

    Routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/blank-components/navbar/navbar.component';
+import { FooterComponent } from './components/blank-components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/blank-components/home/home.component';
+import { BrandsComponent } from './components/blank-components/brands/brands.component';
+import { CartsComponent } from './components/blank-components/carts/carts.component';
+import { ProductsComponent } from './components/blank-components/products/products.component';
+import { CategoriesComponent } from './components/blank-components/categories/categories.component';
+import { LoginComponent } from './components/auth-components/login/login.component';
+import { RegisterComponent } from './components/auth-components/register/register.component';
+import { NotfoundComponent } from './components/blank-components/notfound/notfound.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', redirectTo: 'home',pathMatch:'full'},
+    {path:'home', component: HomeComponent},
+    {path:'brands',component: BrandsComponent },
+    {path:'cart', component: CartsComponent},
+    {path:'products', component: ProductsComponent},
+    {path:'categories', component:CategoriesComponent},
+    {path:'login', component: LoginComponent},
+    {path:'register', component: RegisterComponent},
+    {path:'**', component:NotfoundComponent}
+];
