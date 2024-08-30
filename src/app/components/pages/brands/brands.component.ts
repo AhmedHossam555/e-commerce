ef85391f6c165afc7d484d3cdd34ad7864cda114
ahmed hossam <ahmed.hossam.elgamal77@gmail.com>
2024-08-30 20:58:23 +0300

    brands

--- src/app/components/pages/brands/brands.component.ts
+++ src/app/components/pages/brands/brands.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BrandsService } from '../../../shared/services/brands.service';
 
 @Component({
   selector: 'app-brands',
@@ -7,6 +8,18 @@ import { Component } from '@angular/core';
   templateUrl: './brands.component.html',
   styleUrl: './brands.component.scss'
 })
-export class BrandsComponent {
+export class BrandsComponent implements OnInit{
+  brandsList:any[] = []
+  constructor(private _brands: BrandsService){
 
+  }
+  ngOnInit(): void {
+    this.getAllBrands()
+  }
+  getAllBrands(){
+     this._brands.getAllBrands().subscribe((resp)=>{
+      this.brandsList = resp.data;
+      console.log(resp.data)
+     })
+  }
 }
