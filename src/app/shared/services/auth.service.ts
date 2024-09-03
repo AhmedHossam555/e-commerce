d749fae74ac3a3c6e4b26da79d46a9be5ed06d99
ahmed hossam <ahmed.hossam.elgamal77@gmail.com>
2024-09-04 01:56:18 +0300

    update Auth Service

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,19 +1,18 @@
-import { afterNextRender, Injectable } from '@angular/core';
+import {  Inject, Injectable, PLATFORM_ID } from '@angular/core';
 import { Login, Register, ResetPassword } from '../interface/auth';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { Enviroment } from '../../Base/enviroment';
 import { jwtDecode } from "jwt-decode";
+import { isPlatformBrowser } from '@angular/common';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  
-
-  constructor(private _http: HttpClient) { 
-    afterNextRender(()=>{
+  constructor(private _http: HttpClient, @Inject(PLATFORM_ID) private x: Object) { 
+    if(isPlatformBrowser(x)){
       this.userInformation();
-    })
+    }
   }
   register(formData: Register): Observable<any>{
     return this._http.post(`${Enviroment.baseUrl}/api/v1/auth/signup`, formData);
@@ -34,6 +33,6 @@ export class AuthService {
     return this._http.post(`${Enviroment.baseUrl}/api/v1/auth/verifyResetCode`,resetData)
   }
   ResetPassword(FormData: ResetPassword):Observable<any>{
-    return this._http.put(`${Enviroment.baseUrl}/api/v1/auth/verifyResetCode`,FormData)
+    return this._http.put(`${Enviroment.baseUrl}/api/v1/auth/resetPassword`,FormData)
   }
 }
